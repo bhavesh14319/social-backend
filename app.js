a6550661fb80a0320f9ede26ce3b26b07ae70dc5
bhavesh <bhavesh.mali052002@gmail.com>
2024-02-06 18:25:04 +0530

    reverted

--- app.js
+++ app.js
@@ -14,17 +14,9 @@ dotenv.config();
 app.use(require("cookie-parser")())
 //using middleware
 app.use(cors({
-    origin: ["https://friendsmedia.netlify.app", "http://localhost:3000/"],
-    default : "https://friendsmedia.netlify.app",
+    origin: "https://friendsmedia.netlify.app",
     credentials: true
 }))
-
-app.all('*', function(req, res, next) {
-    const origin = cors.origin.includes(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({limit:"50mb",extended:true }));
