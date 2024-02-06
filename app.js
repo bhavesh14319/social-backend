9b1a72ece52e93dfdb678ff9f03f18b2e873c13b
bhavesh <bhavesh.mali052002@gmail.com>
2024-02-06 18:15:52 +0530

    changes

--- app.js
+++ app.js
@@ -15,8 +15,16 @@ app.use(require("cookie-parser")())
 //using middleware
 app.use(cors({
     origin: ["https://friendsmedia.netlify.app", "http://localhost:3000/"],
+    default : "https://friendsmedia.netlify.app",
     credentials: true
 }))
+
+app.all('*', function(req, res, next) {
+    const origin = cors.origin.includes(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({limit:"50mb",extended:true }));
