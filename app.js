9bdd898608130d40c7d6e6e559cad2ca1f23b509
bhavesh <bhavesh.mali052002@gmail.com>
2024-02-06 18:10:23 +0530

    origin

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(cors({
     origin: "https://friendsmedia.netlify.app",
     credentials: true
 }))
+app.use(cors({
+    origin: "http://localhost:3000/",
+    credentials: true
+}))
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({limit:"50mb",extended:true }));
 
