8059783ecc4c79256ee4c82dc1eebd5c8f246301
bhavesh <bhavesh.mali052002@gmail.com>
2024-02-06 18:14:03 +0530

    multiple origin

--- app.js
+++ app.js
@@ -14,13 +14,10 @@ dotenv.config();
 app.use(require("cookie-parser")())
 //using middleware
 app.use(cors({
-    origin: "https://friendsmedia.netlify.app",
+    origin: ["https://friendsmedia.netlify.app", "http://localhost:3000/"],
     credentials: true
 }))
-app.use(cors({
-    origin: "http://localhost:3000/",
-    credentials: true
-}))
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({limit:"50mb",extended:true }));
 
